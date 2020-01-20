ea3247e819aba8ed137b6687dcf6239189379b5e
jollitycn@163.com <jollitycn@163.com>
2020-01-20 14:14:36 +0800

    update for package
    Signed-off-by: jollitycn@163.com <jollitycn@163.com>

--- ism-project/src/main/java/com/jollitycn/project/ProjectApplication.java
+++ ism-project/src/main/java/com/jollitycn/project/ProjectApplication.java
@@ -1,4 +1,4 @@
-package com.insigma.project;
+package com.jollitycn.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ism-project/src/test/java/com/jollitycn/project/ProjectApplicationTests.java
+++ ism-project/src/test/java/com/jollitycn/project/ProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.insigma.project;
+package com.jollitycn.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
